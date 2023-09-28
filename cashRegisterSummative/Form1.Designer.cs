7d85c4723f0a3cd55ddaf24b74294dd9a081c1d0
Alistair Krieck <aliskrie701@outlook.com>
2023-09-28 11:28:08 -0400

    Changed design, added default amounts to potatoCount, tomatoCount, and brickCount

--- cashRegisterSummative/Form1.Designer.cs
+++ cashRegisterSummative/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GroceryStore));
             this.calculateButton = new System.Windows.Forms.Button();
             this.brickLabel = new System.Windows.Forms.Label();
             this.tomatoLabel = new System.Windows.Forms.Label();
@@ -42,7 +43,6 @@
             this.subtotalOutput = new System.Windows.Forms.Label();
             this.taxOutput = new System.Windows.Forms.Label();
             this.totalCostOutput = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.moneyInput = new System.Windows.Forms.TextBox();
             this.tenderedLabel = new System.Windows.Forms.Label();
             this.changeOutput = new System.Windows.Forms.Label();
@@ -51,11 +51,15 @@
             this.receiptButton = new System.Windows.Forms.Button();
             this.receiptOutput = new System.Windows.Forms.Label();
             this.receiptOutputCover = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // calculateButton
             // 
             this.calculateButton.BackColor = System.Drawing.Color.White;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.ForeColor = System.Drawing.Color.Black;
             this.calculateButton.Location = new System.Drawing.Point(29, 196);
             this.calculateButton.Name = "calculateButton";
             this.calculateButton.Size = new System.Drawing.Size(151, 29);
@@ -67,6 +71,7 @@
             // brickLabel
             // 
             this.brickLabel.AutoSize = true;
+            this.brickLabel.ForeColor = System.Drawing.Color.White;
             this.brickLabel.Location = new System.Drawing.Point(20, 162);
             this.brickLabel.Name = "brickLabel";
             this.brickLabel.Size = new System.Drawing.Size(47, 16);
@@ -77,6 +82,7 @@
             // tomatoLabel
             // 
             this.tomatoLabel.AutoSize = true;
+            this.tomatoLabel.ForeColor = System.Drawing.Color.White;
             this.tomatoLabel.Location = new System.Drawing.Point(20, 118);
             this.tomatoLabel.Name = "tomatoLabel";
             this.tomatoLabel.Size = new System.Drawing.Size(72, 16);
@@ -87,6 +93,7 @@
             // potatoLabel
             // 
             this.potatoLabel.AutoSize = true;
+            this.potatoLabel.ForeColor = System.Drawing.Color.White;
             this.potatoLabel.Location = new System.Drawing.Point(20, 70);
             this.potatoLabel.Name = "potatoLabel";
             this.potatoLabel.Size = new System.Drawing.Size(64, 16);
@@ -96,41 +103,49 @@
             // 
             // potatoInput
             // 
+            this.potatoInput.ForeColor = System.Drawing.Color.Black;
             this.potatoInput.Location = new System.Drawing.Point(92, 67);
             this.potatoInput.Name = "potatoInput";
             this.potatoInput.Size = new System.Drawing.Size(100, 22);
             this.potatoInput.TabIndex = 4;
+            this.potatoInput.Text = "0";
             this.potatoInput.TextChanged += new System.EventHandler(this.potatoInput_TextChanged);
             // 
             // tomatoInput
             // 
+            this.tomatoInput.ForeColor = System.Drawing.Color.Black;
             this.tomatoInput.Location = new System.Drawing.Point(92, 112);
             this.tomatoInput.Name = "tomatoInput";
             this.tomatoInput.Size = new System.Drawing.Size(100, 22);
             this.tomatoInput.TabIndex = 5;
+            this.tomatoInput.Text = "0";
             this.tomatoInput.TextChanged += new System.EventHandler(this.tomatoInput_TextChanged);
             // 
             // brickInput
             // 
+            this.brickInput.ForeColor = System.Drawing.Color.Black;
             this.brickInput.Location = new System.Drawing.Point(92, 159);
             this.brickInput.Name = "brickInput";
             this.brickInput.Size = new System.Drawing.Size(100, 22);
             this.brickInput.TabIndex = 6;
+            this.brickInput.Text = "0";
             this.brickInput.TextChanged += new System.EventHandler(this.brickInput_TextChanged);
             // 
             // label4
             // 
-            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(-8, 322);
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(-8, 310);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(240, 13);
+            this.label4.Size = new System.Drawing.Size(233, 18);
             this.label4.TabIndex = 7;
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // subtotalLabel
             // 
             this.subtotalLabel.AutoSize = true;
-            this.subtotalLabel.Location = new System.Drawing.Point(26, 228);
+            this.subtotalLabel.ForeColor = System.Drawing.Color.White;
+            this.subtotalLabel.Location = new System.Drawing.Point(38, 227);
             this.subtotalLabel.Name = "subtotalLabel";
             this.subtotalLabel.Size = new System.Drawing.Size(59, 16);
             this.subtotalLabel.TabIndex = 10;
@@ -140,7 +155,8 @@
             // taxLabel
             // 
             this.taxLabel.AutoSize = true;
-            this.taxLabel.Location = new System.Drawing.Point(26, 258);
+            this.taxLabel.ForeColor = System.Drawing.Color.White;
+            this.taxLabel.Location = new System.Drawing.Point(38, 257);
             this.taxLabel.Name = "taxLabel";
             this.taxLabel.Size = new System.Drawing.Size(33, 16);
             this.taxLabel.TabIndex = 9;
@@ -150,7 +166,8 @@
             // totalCostLabel
             // 
             this.totalCostLabel.AutoSize = true;
-            this.totalCostLabel.Location = new System.Drawing.Point(26, 289);
+            this.totalCostLabel.ForeColor = System.Drawing.Color.White;
+            this.totalCostLabel.Location = new System.Drawing.Point(38, 288);
             this.totalCostLabel.Name = "totalCostLabel";
             this.totalCostLabel.Size = new System.Drawing.Size(41, 16);
             this.totalCostLabel.TabIndex = 8;
@@ -160,7 +177,8 @@
             // subtotalOutput
             // 
             this.subtotalOutput.AutoSize = true;
-            this.subtotalOutput.Location = new System.Drawing.Point(132, 228);
+            this.subtotalOutput.ForeColor = System.Drawing.Color.White;
+            this.subtotalOutput.Location = new System.Drawing.Point(144, 227);
             this.subtotalOutput.Name = "subtotalOutput";
             this.subtotalOutput.Size = new System.Drawing.Size(15, 16);
             this.subtotalOutput.TabIndex = 13;
@@ -170,7 +188,8 @@
             // taxOutput
             // 
             this.taxOutput.AutoSize = true;
-            this.taxOutput.Location = new System.Drawing.Point(132, 258);
+            this.taxOutput.ForeColor = System.Drawing.Color.White;
+            this.taxOutput.Location = new System.Drawing.Point(144, 257);
             this.taxOutput.Name = "taxOutput";
             this.taxOutput.Size = new System.Drawing.Size(15, 16);
             this.taxOutput.TabIndex = 12;
@@ -180,30 +199,18 @@
             // totalCostOutput
             // 
             this.totalCostOutput.AutoSize = true;
-            this.totalCostOutput.Location = new System.Drawing.Point(132, 289);
+            this.totalCostOutput.ForeColor = System.Drawing.Color.White;
+            this.totalCostOutput.Location = new System.Drawing.Point(144, 288);
             this.totalCostOutput.Name = "totalCostOutput";
             this.totalCostOutput.Size = new System.Drawing.Size(15, 16);
             this.totalCostOutput.TabIndex = 11;
             this.totalCostOutput.Text = "X";
             this.totalCostOutput.Click += new System.EventHandler(this.label10_Click);
             // 
-            // label5
-            // 
-            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label5.Location = new System.Drawing.Point(-1, -6);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(601, 52);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "Grocery and Brick Store";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
             // moneyInput
             // 
-            this.moneyInput.Location = new System.Drawing.Point(101, 338);
+            this.moneyInput.ForeColor = System.Drawing.Color.Black;
+            this.moneyInput.Location = new System.Drawing.Point(89, 344);
             this.moneyInput.Name = "moneyInput";
             this.moneyInput.Size = new System.Drawing.Size(100, 22);
             this.moneyInput.TabIndex = 16;
@@ -212,7 +219,8 @@
             // tenderedLabel
             // 
             this.tenderedLabel.AutoSize = true;
-            this.tenderedLabel.Location = new System.Drawing.Point(28, 341);
+            this.tenderedLabel.ForeColor = System.Drawing.Color.White;
+            this.tenderedLabel.Location = new System.Drawing.Point(16, 347);
             this.tenderedLabel.Name = "tenderedLabel";
             this.tenderedLabel.Size = new System.Drawing.Size(67, 16);
             this.tenderedLabel.TabIndex = 15;
@@ -222,7 +230,8 @@
             // changeOutput
             // 
             this.changeOutput.AutoSize = true;
-            this.changeOutput.Location = new System.Drawing.Point(156, 405);
+            this.changeOutput.ForeColor = System.Drawing.Color.White;
+            this.changeOutput.Location = new System.Drawing.Point(144, 411);
             this.changeOutput.Name = "changeOutput";
             this.changeOutput.Size = new System.Drawing.Size(15, 16);
             this.changeOutput.TabIndex = 18;
@@ -232,7 +241,8 @@
             // changeLabel
             // 
             this.changeLabel.AutoSize = true;
-            this.changeLabel.Location = new System.Drawing.Point(52, 405);
+            this.changeLabel.ForeColor = System.Drawing.Color.White;
+            this.changeLabel.Location = new System.Drawing.Point(40, 411);
             this.changeLabel.Name = "changeLabel";
             this.changeLabel.Size = new System.Drawing.Size(83, 16);
             this.changeLabel.TabIndex = 17;
@@ -242,7 +252,9 @@
             // calculateChangeButton
             // 
             this.calculateChangeButton.BackColor = System.Drawing.Color.White;
-            this.calculateChangeButton.Location = new System.Drawing.Point(41, 366);
+            this.calculateChangeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateChangeButton.ForeColor = System.Drawing.Color.Black;
+            this.calculateChangeButton.Location = new System.Drawing.Point(29, 372);
             this.calculateChangeButton.Name = "calculateChangeButton";
             this.calculateChangeButton.Size = new System.Drawing.Size(151, 29);
             this.calculateChangeButton.TabIndex = 19;
@@ -253,7 +265,8 @@
             // receiptButton
             // 
             this.receiptButton.BackColor = System.Drawing.Color.White;
-            this.receiptButton.Location = new System.Drawing.Point(41, 434);
+            this.receiptButton.ForeColor = System.Drawing.Color.Black;
+            this.receiptButton.Location = new System.Drawing.Point(29, 440);
             this.receiptButton.Name = "receiptButton";
             this.receiptButton.Size = new System.Drawing.Size(151, 29);
             this.receiptButton.TabIndex = 20;
@@ -265,29 +278,42 @@
             // 
             this.receiptOutput.BackColor = System.Drawing.Color.White;
             this.receiptOutput.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptOutput.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.receiptOutput.Location = new System.Drawing.Point(231, 46);
             this.receiptOutput.Name = "receiptOutput";
-            this.receiptOutput.Size = new System.Drawing.Size(369, 497);
+            this.receiptOutput.Size = new System.Drawing.Size(369, 479);
             this.receiptOutput.TabIndex = 21;
             this.receiptOutput.Click += new System.EventHandler(this.receiptOutput_Click);
             // 
             // receiptOutputCover
             // 
-            this.receiptOutputCover.BackColor = System.Drawing.Color.Gainsboro;
-            this.receiptOutputCover.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.receiptOutputCover.Location = new System.Drawing.Point(231, 46);
+            this.receiptOutputCover.BackColor = System.Drawing.Color.Black;
+            this.receiptOutputCover.ForeColor = System.Drawing.SystemColors.Control;
+            this.receiptOutputCover.Location = new System.Drawing.Point(231, 47);
             this.receiptOutputCover.Name = "receiptOutputCover";
-            this.receiptOutputCover.Size = new System.Drawing.Size(369, 479);
+            this.receiptOutputCover.Size = new System.Drawing.Size(377, 479);
             this.receiptOutputCover.TabIndex = 22;
             this.receiptOutputCover.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.receiptOutputCover.Click += new System.EventHandler(this.receiptOutputCover_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Image = global::cashRegisterSummative.Properties.Resources.icon_2_;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(605, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
             // 
             // GroceryStore
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(587, 518);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(587, 507);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.receiptOutputCover);
             this.Controls.Add(this.receiptOutput);
             this.Controls.Add(this.receiptButton);
@@ -296,7 +322,6 @@
             this.Controls.Add(this.changeLabel);
             this.Controls.Add(this.moneyInput);
             this.Controls.Add(this.tenderedLabel);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.subtotalOutput);
             this.Controls.Add(this.taxOutput);
             this.Controls.Add(this.totalCostOutput);
@@ -311,10 +336,12 @@
             this.Controls.Add(this.tomatoLabel);
             this.Controls.Add(this.brickLabel);
             this.Controls.Add(this.calculateButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GroceryStore";
             this.Tag = "";
-            this.Text = "Form1";
+            this.Text = "Grocery and Brick Store";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -336,7 +363,6 @@
         private System.Windows.Forms.Label subtotalOutput;
         private System.Windows.Forms.Label taxOutput;
         private System.Windows.Forms.Label totalCostOutput;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox moneyInput;
         private System.Windows.Forms.Label tenderedLabel;
         private System.Windows.Forms.Label changeOutput;
@@ -345,6 +371,7 @@
         private System.Windows.Forms.Button receiptButton;
         private System.Windows.Forms.Label receiptOutput;
         private System.Windows.Forms.Label receiptOutputCover;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- cashRegisterSummative/Properties/Resources.Designer.cs
+++ cashRegisterSummative/Properties/Resources.Designer.cs
@@ -61,6 +61,26 @@ namespace cashRegisterSummative.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_2_ {
+            get {
+                object obj = ResourceManager.GetObject("icon(2)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon_lVy_icon {
+            get {
+                object obj = ResourceManager.GetObject("icon_lVy_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream printSound {
